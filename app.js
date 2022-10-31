f7f0aaeff72a0103d90cbe0416646284e9f5e66e
cebrail-pekoz <pkzcbrl@gmail.com>
2022-10-31 11:10:41 +0200

    template

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
-const express = require('express');
+// ***** MODÜLLER
+const express = require('express'); // node.js uygulama çatısı
+const ejs = require('ejs'); // template engine
+// ***** //MODÜLLER
 
-const PORT = 3000;
-const LOCAL = '127.0.0.1';
-const app = express();
+// ***** UYGULAMA DEĞİŞKENLERİ
+const PORT = 3000; // çalışılacak port
+const LOCAL = '127.0.0.1'; // localhost
+const app = express(); // express uygulaması instance
+// ***** //UYGULAMA DEĞİŞKENLERİ
 
+// ***** TEMPLATE ENGINE
+app.set('view engine', 'ejs'); // template engine ekleme
+app.use(express.static('public')); // statik dosyaların yolu  -middleware
+// ***** //TEMPLATE ENGINE
+
+// ***** YÖNLENDİRMELER
+// anasayfa isteği
 app.get('/', (req, res) => {
-    res.status(200).send('Index Sayfası');
+    res.status(200).render('index', {
+        page_name: 'index',
+    });
 });
 
+// about sayfası
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
+});
+// ***** //YÖNLENDİRMELER
+
+// ***** SERVER
+// local server çalıştırma
 app.listen(PORT, () => {
     console.log(`Server Çalışıyor : http://${LOCAL}:${PORT}`);
 });
+// ***** //SERVER
